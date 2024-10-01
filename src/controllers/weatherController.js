acdaccbd54f1a895c346f3c457dc3e66f696bc30
Serena <sxl1e23@soton.ac.uk>
2024-10-01 23:28:50 +0100

    done basic server that returns tempterature based on given location, but forgot git add before

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -1,8 +1,47 @@
 import express from "express";
+import dotenv from "dotenv";
+import NodeGeocoder from "node-geocoder";
+import axios from "axios";
+dotenv.config();
+const options = {
+    provider: 'locationiq',
+    apiKey: process.env.LOCATION_TO_IP_API
+};
+
+const geoCoder = NodeGeocoder(options);
+
+
 /**
  * gets weather for given location
  * @param req location
  * @param res weather at location
  * @returns {Promise<express.Response>}
  */
-export const getWeather = async (req, res) => {return res.send("weather")}
+export const getWeather = async (req, res) => {
+    const {city,country} = req.body;
+
+
+
+    const result = await geoCoder.geocode(`${city}, ${country}`);
+    if (!result){
+        return res.status(404).send("city not found");
+    }
+
+    const lat = result[0].latitude;
+    const long = result[0].longitude;
+    const url = "https://api.open-meteo.com/v1/forecast"
+    const params = {
+    "latitude": lat,
+        "longitude": long,
+        "hourly": "temperature_2m"
+    }
+    const weather = await axios.get(url, {params});
+
+    const response = {
+        temperature: weather.data.hourly.temperature_2m[0],
+        time_GMT: weather.data.hourly.time[0],
+    }
+    console.log(response)
+
+    return res.status(200).json(response);
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import weatherRoutes from "./routes/weatherRoutes.js";
+import bodyParser from "body-parser";
 dotenv.config();
 /**
  * Instance of express
  * @type {Express}
  */
 const app = express();
+app.use(bodyParser.json());
 /**
  * port number
  * @type {number}
